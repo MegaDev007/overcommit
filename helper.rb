MegaDev007/overcommit

commit 750b64e6b9a41db1affb1422da2a2c787bf868c1

Add support for linking scripts directory
Previously, we only ever copied the `scripts` directory, even if an
"install" was specified instead of an explicit copy.

This commit adds support for symlinking the scripts directory, so that
any changes to it are automatically propagated to the installed hooks.

Related Story:
  https://www.pivotaltracker.com/story/show/36008171
  ("Add git hook for auto-indexing tags for repo")

Change-Id: Ife5816b21da435334c3e3ea26a73d45848bfdc4f
Reviewed-on: https://gerrit.causes.com/12252
Reviewed-by: Shane da Silva <shane@causes.com>
Tested-by: Shane da Silva <shane@causes.com>

helper.rb
@@ -36,6 +36,7 @@ module Helper
     puts "  Installing hooks to #{repo}..."
     HOOKS.each do |hook|
       print "    #{hook}..."
+      FileUtils.rm target_hook_path(repo, hook)
       if options[:method] == :copy
         FileUtils.cp    hook_path(hook), target_hook_path(repo, hook)
       else
@@ -46,9 +47,14 @@ module Helper
     end
   end
 
-  def copy_scripts(repo)
+  def copy_scripts(repo, options = {})
     print '    helper scripts...'
-    FileUtils.cp_r SCRIPTS, target_script_path(repo)
+    FileUtils.rm_rf target_script_path(repo)
+    if options[:method] == :copy
+      FileUtils.cp_r SCRIPTS, target_script_path(repo)
+    else
+      FileUtils.ln_sf scripts_path, target_script_path(repo)
+    end
     success 'OK'
   end
 
@@ -58,6 +64,10 @@ module Helper
 
   def hook_path(hook)
     File.expand_path(File.join('hooks', hook))
+  end
+
+  def scripts_path
+    File.expand_path(SCRIPTS)
   end
 
   def repos
@@ -77,6 +87,6 @@ module Helper
   end
 
   def target_script_path(repo)
-    File.join(repo_path(repo), HOOKS_PATH)
+    File.join(repo_path(repo), HOOKS_PATH, 'scripts')
   end
 end

Rakefile
@@ -12,7 +12,7 @@ def install_repos(options = {})
       next
     end
     copy_hooks(repo, options)
-    copy_scripts(repo) if options[:method] == :copy
+    copy_scripts(repo, options)
   end
   puts
 end

commit 48d80d15a14b877632776204bae7d121ce1223e2

Fix install script for hooks which do not yet exist
Previously, running the install script would fail if a repo did not
already have a script for a particular commit hook in its `.git/hooks`
directory.

This was caused by commit 750b64e, which added a call to `FileUtils.rm`,
which fails if a file does not exist. The fix was to change this to
`FileUtils.rm_f`, so that it does not fail if the file does not exist.

Change-Id: I16ed4418962824b48c0772b34a7bc8ba0558d390
Reviewed-on: https://gerrit.causes.com/12276
Tested-by: Shane da Silva <shane@causes.com>
Reviewed-by: Kim Douglas Clarence Mason <kim.mason@causes.com>

helper.rb
@@ -36,7 +36,7 @@ module Helper
     puts "  Installing hooks to #{repo}..."
     HOOKS.each do |hook|
       print "    #{hook}..."
-      FileUtils.rm target_hook_path(repo, hook)
+      FileUtils.rm_f target_hook_path(repo, hook)
       if options[:method] == :copy
         FileUtils.cp    hook_path(hook), target_hook_path(repo, hook)
       else